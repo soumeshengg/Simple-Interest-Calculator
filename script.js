47c55be400b0aa6ef72025bf9627b45929dc0443
Soumesh Bhaumik <soumeshbhaumik99@gmail.com>
2021-02-06 16:46:06 +0530

    Some changes are made. Still certain area are not covered. Later those areas will be reviewed.

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 function compute()
 {
-    let p = parseFloat(document.getElementById("principal").value);
-    var valid = validation(p);
-    if(valid == true) {
-        let r = parseFloat(document.getElementById("rate").value);
+
+    if (validate()) {
+        var p = parseFloat(document.getElementById("principal").value);
+        var r = parseFloat(document.getElementById("rate").value);
         var y = parseInt(document.getElementById("years").value);
+
         var principal = p;
         var rate = r;
         var date = new Date();
         var years = y;
         var final_year = y + date.getFullYear();
+
         var interest = principal * years * rate / 100;
-        document.getElementById("result").innerHTML = "If you deposit " + p + ", " +
-            "at an interest rate of " + r +
-            "%. You will receive an amount of " + interest +
-            ", in the year " + final_year + ".";
+        document.getElementById("result").innerHTML = "If you deposit " +<mark>p</mark> + ","+"<br>" +
+            "at an interest rate of " +"<mark>r</mark>"+ "<mark>%</mark>."+"<br>"+
+            "You will receive an amount of " + "<mark>interest</mark>"+","+"<br>"+
+            "in the year " + "<mark>final_year</mark>" + ".";
+
     }
     else {
-        validation(p);
+        document.getElementById("principal").focus();
     }
-
 }
 
 function slider(r) {
-    var output = document.getElementById("ran");
+    const output = document.getElementById("slider_range");
     output.innerHTML = r+"%";
 }
 
-function validation(pr) {
-    if(pr <= 0){
-        alert("Enter a valid number");
-        maindiv.amount.focus();
+function validate()
+ {
+     var money = document.getElementById("principal").value;
+     if (money <=0 || money==="" || money==null){
+        alert("Enter a positive number");
         return false;
     }
     else
